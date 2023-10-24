ac80fd8183fa7ed32951998f2d10d118464b9e26
Kolzera <148531141+Kolzera@users.noreply.github.com>
2023-10-24 15:06:00 +0400

    Add files via upload

---
+++ lab_8/WpfApp2/Window0.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void LifeEvents(object sender, RoutedEventArgs e)
+        {
+            Window1 wnd1 = new Window1();
+            wnd1.ShowInTaskbar = false; // Не показывать в панели задач
+            wnd1.ShowDialog();          // Показать в модальном режиме
+        }
+    }
+    
+
+}
