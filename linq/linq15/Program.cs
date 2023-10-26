dc33bab84ac0770e25ec4cd01e8e28363a8812ff
Kolzera <148531141+Kolzera@users.noreply.github.com>
2023-10-26 17:18:59 +0400

    Add files via upload

---
+++ linq/linq15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double factorial = Enumerable.Range(1, n).Aggregate(1.0, (acc, x) => acc * x);
+
+            Console.WriteLine($"Факториал числа {n} равен {factorial}");
+        }
+    }
+}
---
+++ linq/linq31/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Intersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число K:");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите последовательность строк A:");
+            string[] a = Console.ReadLine().Split();
+
+            var intersection = a.Take(k).Intersect(a.SkipWhile(s => !char.IsDigit(s.Last())));
+
+            var sortedIntersection = intersection.OrderBy(s => s.Length).ThenBy(s => s);
+
+            Console.WriteLine("Результат:");
+            foreach (var item in sortedIntersection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
